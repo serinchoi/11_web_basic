7e12215592d3d91db37565e5c3909e441efee370
17_web_rin <17_web_rin@DESKTOP-2CHQOPD>
2022-04-06 21:43:16 +0900

    DATA_BASE_3 수업

---
+++ 11_web_basic/WebContent/05_MYSQL/mysqlEx13_문제.sql
@@ -0,0 +1,25 @@
+# 문제 1) 사원번호 , 부서번호 , 근무 부서 이름을 가져오기. (DEPT_EMP , DEPARTMENTS)
+
+
+
+# 문제 2) 사원번호 , 이름(first,last) , 직함을 조회하기. (EMPLOYEES, TITLES)
+
+
+    
+# 문제 3) 사원번호 , 사원이름(first_name,last_name) , 부서번호 , 부서이름을 조회하기. (EMPLOYEES , DEPT_EMP , DEPARTMENTS)
+
+
+
+# 문제 4) 사원번호 , 이름(first,last) , 부서 번호를 조회하기. (EMPLOYEES , DEPT_EMP)
+
+
+
+# 문제 5) 사원번호 , 이름 , 근무 부서 이름을 조회하기. (EMPLOYEES , DEPT_EMP , DEPARTMENTS)
+
+
+
+# 문제 6) 부서별로 그룹화하고 부서이름 , 부서번호 , 직원수를 조회하고 직원수가 많은 곳부터 조회하기. (DEPT_EMP , DEPARTMENTS)
+
+
+
+# 문제 7) 부서별로 그룹화하고 부서별안에 성별을 그룹화하여 직원 수를 조회하기. (DEPT_EMP , DEPARTMENTS , EMPLOYEES)
